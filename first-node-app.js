1f8afc2436abf4ee849c64814e83c1f417417a4a
Rishav Vashisht <rishavvashisht845@gmail.com>
2020-07-08 04:04:01 -0400

    commiting

--- first-node-app.js
+++ first-node-app.js
@@ -1,1 +1,26 @@
 const express = require('express');
+const { request, response } = require('express');
+const app = express();
+const https = require('https');
+
+app.get('/' , (request, response) => {
+https.get('https://insult.mattbas.org/api/insult', resp => {
+resp.on('data', chunk => {
+response.send(`
+      <http>
+          <head><title>Apple</title></head>
+
+          <body>
+            <center><h1>Hola!</h1></center>
+            
+            <p style="font-size: 24px; color: black;">
+                ${chunk.toString()}!
+            </p>
+          </body>
+      </http>
+      `);
+    });
+  });
+});
+
+app.listen(3000);
